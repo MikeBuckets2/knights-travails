b70a4fb63403494ac6c7fd233b8d3592c7df10eb
Michael Morris <michaelmorris121@yahoo.com>
2026-02-05 09:01:06 -0600

    add new moves to queue

--- script.js
+++ script.js
@@ -18,11 +18,27 @@ function knightMoves(start, end) {
     while (queue.length > 0) {
         const { position, path } = queue.shift();
         const [x, y] = position;
+
+        if (x === end[0] && y === end[1]) {
+            console.log(`You made it in ${path.length - 1} moves! Here's your path:`);
+            path.forEach(square => console.log(square));
+            return path;
+        }
+
+        for (const [dx, dy] of moves) {
+            const newX = x + dx;
+            const newY = y + dy;
+            const key = [nexX, newY].toString();
+
+            if (isValid(newX, newY) && !visited.has(key)) {
+                visited.add(key);
+                queue.push({
+                    position: [newX, newY], 
+                    path: [...path, [newX, newY]],
+                });
+            }
+        }
     }
 
-    if (x === end[0] && y === end[1]) {
-        console.log(`You made it in ${path.length - 1} moves! Here's your path:`);
-        path.forEach(square => console.log(square));
-        return path;
-    }
+    return null;
 };
